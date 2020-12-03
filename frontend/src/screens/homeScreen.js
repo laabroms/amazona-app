f2845a712f4e12ca0de27532198b06e73964986d
Lucas Abroms <lucasabroms@lucass-mbp.home>
2020-12-02 21:42:06 -0800

    added redux to homescreen

--- frontend/src/screens/homeScreen.js
+++ frontend/src/screens/homeScreen.js
@@ -1,30 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import data from "../data";
+import React, { useEffect } from 'react';
 import Product from "../components/product";
 import MessageBox from '../components/messageBox';
 import LoadingBox from '../components/loadingBox';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 
 export default function HomeScreen() {
-    const [products, setProducts] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
+    const dispatch = useDispatch();
+    const productList = useSelector( (state) => state.productList );
+    const { loading, error, products } = productList;
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                setLoading(true);
-                const { data } = await axios.get("/api/products");
-                setLoading(false);
-                setProducts(data);
-            } 
-            catch (err) {
-                setError(err.message);
-                setLoading(false);
-            }
-            
-        };
-        fetchData();
-    }, [])
+        dispatch(listProducts());
+    }, []);
 
     return (
       <div>
@@ -34,7 +21,7 @@ export default function HomeScreen() {
       ) : (
 
         <div className="row center">
-          {data.products.map((product) => (
+          {products.map((product) => (
             <Product key={product._id} product={product} />
           ))}
         </div>
