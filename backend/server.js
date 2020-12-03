6ccb726ad549f3d6a9fd3d3c3afab74db6f3bf9a
Lucas Abroms <lucasabroms@lucass-mbp.home>
2020-12-03 08:55:05 -0800

    redux added for details screen

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,16 @@ import express from 'express';
 import data from './data.js';
 
 const app = express();
-const port = process.env.PORT || 5000;
+
+app.get('/api/products/:id', (req, res) => {
+    const product = data.products.find( (x) => x._id === req.params.id);
+    if (product) {
+        res.send(product);
+    }
+    else {
+        res.status(404).send({message: 'Product not Found'});
+    }
+})
 
 app.get('/api/products', (req, res) => {
     res.send(data.products);
@@ -11,6 +20,9 @@ app.get('/api/products', (req, res) => {
 app.get('/', (req, res) => {
     res.send('Server is ready');
 });
+// eslint-disable-next-line no-undef
+const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
     console.log(`Serve at http://localhost:${port}`);
 });
