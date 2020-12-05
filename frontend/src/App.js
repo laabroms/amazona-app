a8ab4a5e40bfed121860870afe81f5cc4b2da0b0
Lucas Abroms <lucasabroms@lucass-mbp.home>
2020-12-04 23:10:02 -0800

    redux implemented for adding to cart, connected to db, sign in screen made

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,40 @@
 import React from 'react';
 
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import ProductScreen from './screens/productScreen';
 import HomeScreen from './screens/homeScreen';
+import CartScreen from './screens/cartScreen';
+import { useSelector } from 'react-redux';
+import SignInScreen from './screens/signInScreen';
 
 
 function App() {
+
+  const cart = useSelector(state => state.cart);
+  const { cartItems } = cart;
+
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a className="brand" href="/">
+            <Link className="brand" to="/">
               Amazona
-            </a>
+            </Link>
           </div>
           <div>
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign In</a>
+            <Link to="/cart">Cart
+            {cartItems.length > 0 && (
+              <span className='badge'>{cartItems.length}</span>
+            )}
+            </Link>
+            <Link to="/signin">Sign In</Link>
           </div>
         </header>
         <main>
+          <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/signin' component={SignInScreen} />
           <Route path='/' component={HomeScreen} exact />
         </main>
         <footer className="row center">All Rights Reserved.</footer>
